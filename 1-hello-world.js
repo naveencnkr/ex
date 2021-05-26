0a551f3a8ab09489ca434a5ad21e62dd1cfcb271
naveencnkr <naveenchappidi1318@gmail.com>
2021-05-26 12:44:02 +0530

    first checkin

---
+++ 1-hello-world.js
@@ -0,0 +1,15 @@
+var i=setInterval(fn,1000);
+var count=0;
+function fn(){
+    console.log("hello puka");
+    count++;
+    if(count>3)
+    {
+        fn1();
+    }
+    
+}
+function fn1(){
+    console.log("done");
+    clearInterval(i);
+}
